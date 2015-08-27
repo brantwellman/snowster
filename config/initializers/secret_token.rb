ea78ab101f0a7949cc1db49fb3425692814b41b3
Brant Wellman <brantwellman@gmail.com>
2015-08-27 06:18:23 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snowster::Application.config.secret_key_base = '77e14776b565c1ab7ccadd16873b3ed1dab177569466ca4654dab00ce61b6b283a6dc4cb0b4152b4c5e99ac0753764d7f94e93b719f424c8a0f8257004f1fe14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snowster::Application.config.session_store :cookie_store, key: '_snowster_session'
